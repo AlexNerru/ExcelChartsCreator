2491bffe9cfeb58261ca49c5ebd31344ec52ad74
Alexander Karpin <Karpin_Aleksandr_1999@mail.ru>
2017-12-12 22:41:29 +0300

    Добавьте файлы проекта.

---
+++ CHW/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace CHW
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.timer2 = new System.Windows.Forms.Timer(this.components);
+			this.button3 = new System.Windows.Forms.Button();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// timer1
+			// 
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+			this.label1.Font = new System.Drawing.Font("Segoe Script", 25F);
+			this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+			this.label1.Location = new System.Drawing.Point(234, 548);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(3257, 266);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Добро пожаловать! \nВы просматриваете КДЗ Карпина Александра Николаевича БПИ176";
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(0, 0);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(100, 23);
+			this.label2.TabIndex = 3;
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+			this.button1.Font = new System.Drawing.Font("Segoe Script", 21.9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.button1.Location = new System.Drawing.Point(1820, 1190);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(656, 230);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "Закрыть!";
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+			this.button2.Font = new System.Drawing.Font("Segoe Script", 21.9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.button2.Location = new System.Drawing.Point(1820, 405);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(656, 230);
+			this.button2.TabIndex = 4;
+			this.button2.Text = "Открыть!";
+			this.button2.UseVisualStyleBackColor = false;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// timer2
+			// 
+			this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+			// 
+			// button3
+			// 
+			this.button3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+			this.button3.Font = new System.Drawing.Font("Segoe Script", 21.9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.button3.Location = new System.Drawing.Point(1820, 817);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(656, 230);
+			this.button3.TabIndex = 5;
+			this.button3.Text = "На помощь!";
+			this.button3.UseVisualStyleBackColor = false;
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ActiveCaptionText;
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.GridColor = System.Drawing.SystemColors.ActiveCaptionText;
+			this.dataGridView1.Location = new System.Drawing.Point(-8, 515);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.RowTemplate.Height = 40;
+			this.dataGridView1.Size = new System.Drawing.Size(3795, 1547);
+			this.dataGridView1.TabIndex = 6;
+			this.dataGridView1.Visible = false;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoSize = true;
+			this.BackColor = System.Drawing.SystemColors.Desktop;
+			this.ClientSize = new System.Drawing.Size(3788, 2072);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.dataGridView1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Name = "Form1";
+			this.Text = "Замечательное КДЗ";
+			this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Timer timer2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.DataGridView dataGridView1;
+	}
+}
+
---
+++ CHW/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Excel = Microsoft.Office.Interop.Excel;
+
+
+namespace CHW
+{
+	public partial class Form1 : Form
+	{
+		private int counter = 0;
+		private const int GREETINGS_TICK_TIME = 1000;
+
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void ChangeMenuState(bool state)
+		{
+			button1.Visible = state;
+			button2.Visible = state;
+			button3.Visible = state;
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			timer1.Enabled = false;  // таймер включен
+			timer2.Interval = GREETINGS_TICK_TIME;
+			timer2.Enabled = true;
+			ChangeMenuState(false);			
+		}
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+				string temp = label1.Text;
+				if (temp.Length > 0)
+					label1.Text = temp.Substring(0, temp.Length - 1);
+				else
+				{
+					timer1.Enabled = false;
+					ChangeMenuState(true);
+				}
+		}	
+		
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+
+		private void timer2_Tick(object sender, EventArgs e)
+		{
+				timer2.Enabled = false;
+				timer1.Enabled = true;
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			string filePath = string.Empty;
+			string fileExt = string.Empty;
+			openFileDialog1.Filter = "Excel Files| *.xls; *.xlsx; *.xlsm";
+			openFileDialog1.ShowDialog();
+		}
+
+		private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+		{
+			string filePath = string.Empty;
+			string fileExt = string.Empty;
+			try
+			{
+				filePath = openFileDialog1.FileName; //get the path of the file  
+				fileExt = Path.GetExtension(filePath); //get the file extension  
+				if (fileExt.CompareTo(".xls") == 0 || fileExt.CompareTo(".xlsx") == 0)
+				{
+					LoadFile(filePath);
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+			}
+
+			private void LoadFile(string filePath)
+			{
+				System.Data.OleDb.OleDbConnection MyConnection;
+				System.Data.DataSet DtSet;
+				System.Data.OleDb.OleDbDataAdapter MyCommand;
+				MyConnection = new System.Data.OleDb.OleDbConnection("provider=Microsoft.Jet.OLEDB.4.0;" +
+					$"Data Source='{filePath}';Extended Properties=Excel 8.0;");
+				// :TODO Сделать, чтобы был выбор листа
+				MyCommand = new System.Data.OleDb.OleDbDataAdapter("select * from [Worksheet$]", MyConnection);
+				DtSet = new System.Data.DataSet();
+				//this.ShowDialog 
+				MyCommand.Fill(DtSet);
+				dataGridView1.DataSource = DtSet.Tables[0];
+				button1.Visible = false;
+				button2.Visible = false;
+				button3.Visible = false;
+				dataGridView1.Visible = true;
+				MyConnection.Close();
+			}
+		}
+	}
+}
+
